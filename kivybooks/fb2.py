a3e4ead4b81e85a3d19569784c197045dd811ef3
stPhoenix <bohdansapovsky@gmail.com>
2018-03-21 22:53:09 +0200

    Refactoring rev1

--- kivybooks/fb2.py
+++ kivybooks/fb2.py
@@ -7,11 +7,30 @@ import base64
 from bs4 import BeautifulSoup as Bs
 import time
 from bs4.element import NavigableString as NV
+from functools import wraps
+
+import logging
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO, filename='debug.log')
+
+logger = logging.getLogger(__name__)
 
 ALIGN = {'center': 'center', 'left': 'left', 'right': 'right'}
 
 
-#############################
+def recursive(func):
+    @wraps(func)
+    def wrapped(self, soup):
+        line = LineModel()
+        bbcode = []
+        func(self, soup, line, bbcode)
+        return self.finish(soup, line, bbcode)
+
+    return wrapped
+
+
 class FB2Parser:
     global ALIGN
 
@@ -21,225 +40,267 @@ class FB2Parser:
         self.__font_size = int(font_size)
         self.__title_font_size = self.__font_size + 12
         self.__subtitle_font_size = self.__font_size + 8
-        #logging.info('Init success')
+        self.__setup_markdown()
+
+    def __setup_markdown(self):
+        # TODO: Add link engine
+        # TODO: Add table engine
+        self.__markdown = {'body': self.body,
+                           'section': self.section,
+                           'description': self.description,
+                           'binary': self.binary,
+                           'FictionBook': self.fiction_book,
+                           '[document]': self.document,
+                           'title': self.title,
+                           'epigraph': self.epigraph,
+                           'subtitle': self.subtitle,
+                           'annotation': self.annotation,
+                           'p': self.p,
+                           'empty-line': self.empty_line,
+                           'strong': self.strong,
+                           'emphasis': self.emphasis,
+                           'sub': self.sub,
+                           'sup': self.sup,
+                           'strikethrough': self.strikethrough,
+                           'code': self.code,
+                           'poem': self.poem,
+                           'stanza': self.stanza,
+                           'v': self.v,
+                           'text-author': self.text_author,
+                           'date': self.date,
+                           'cite': self.cite,
+                           'image': self.image}
 
     def get_text(self):
         return self.__text
 
     def start(self):
         self.prepare(self.__soup)
+        self.clear_text(2)
 
     def prepare(self, soup):
         line = LineModel()
         bbcode = []
-        if soup.name == 'body':
-            for child in soup.children:
-                self.prepare(child)
-        elif soup.name == 'section':
-            line.set_tag('section')
-            #TODO add sections
-            # if soup.get('id') is not None:
-            #     line.set_text(soup['id'])
-            # self.__text.append(line)
-            for child in soup.children:
-                self.prepare(child)
-        elif soup.name == 'description':
-            pass
-        elif soup.name == 'binary':
-            pass
-        elif soup.name == 'FictionBook':
-            for child in soup.children:
-                self.prepare(child)
-        elif soup.name == '[document]':
-            for child in soup.children:
-                self.prepare(child)
-            # This func for removing empty TEXT strings
-            self.clear_text(2)
+        if soup.name in self.__markdown.keys():
+            return self.__markdown[soup.name](soup)
         else:
-            if soup.name == 'title':
-                bbcode.append('[size=%s]' % self.__title_font_size)
-                bbcode.append('[/size]')
-            elif soup.name == 'subtitle':
-                bbcode.append('[size=%s]' % self.__subtitle_font_size)
-                bbcode.append('[/size]')
-            elif soup.name == 'epigraph':
-                line.set_align(ALIGN['right'])
-                bbcode.append('[i]')
-                bbcode.append('[/i]')
-            elif soup.name == 'image':
-                # print('IMAGE FOUND!')
-                href = soup.get('l:href')
-                #print('href is %s' % href)
-                if href is None:
-                    href = soup.get('xlink:href')
-                    #print('href is %s' % href)
-                if href[0] == "#":
-                    href = href.strip('#')
-                    cover_image = self.__soup.find('binary', attrs={'id': href})
-                    if cover_image is not None:
-                        cover_image = cover_image.get_text().strip('\n')
-                        #line.set_text(BytesIO(base64.standard_b64decode(cover_image)))
-                        # print("IMAGE BIN: %s" % cover_image)
-                    else:
-                        cover_image = 'default'
-                    line.set_text(cover_image)
+            return self.finish(soup, line, bbcode)
 
-            elif soup.name == 'annotation':
-                bbcode.append('[i]')
-                bbcode.append('[/i]')
-
-            elif soup.name == 'p':
-                bbcode.append('\t')
-                # To get not None
-                bbcode.append(' ')
-            elif soup.name == 'empty-line':
-                bbcode.append('\n')
-                # To get not None
-                bbcode.append(' ')
-            elif soup.name == 'strong':
-                bbcode.append('[b]')
-                bbcode.append('[/b]')
-            elif soup.name == 'emphasis':
-                bbcode.append('[i]')
-                bbcode.append('[/i]')
-            elif soup.name == 'sub':
-                bbcode.append('[sub]')
-                bbcode.append('[/sub]')
-            elif soup.name == 'sup':
-                bbcode.append('[sup]')
-                bbcode.append('[/sup]')
-            elif soup.name == 'strikethrough':
-                bbcode.append('[strikethrough]')
-                bbcode.append('[/strikethrough]')
-            elif soup.name == 'code':
-                bbcode.append('[code]')
-                bbcode.append('[/code]')
-            elif soup.name == 'poem':
-                line.set_align(ALIGN['center'])
-            elif soup.name == 'stanza':
-                bbcode.append('\n')
-                bbcode.append(' ')
-            elif soup.name == 'v':
-                line.set_align(ALIGN['center'])
-                bbcode.append('\n')
-                bbcode.append(' ')
-            elif soup.name == 'text-author':
-                line.set_align(ALIGN['right'])
-                bbcode.append('\n')
-                bbcode.append(' ')
-            elif soup.name == 'date':
-                line.set_align(ALIGN['right'])
-                bbcode.append('\n')
-                bbcode.append(' ')
-            elif soup.name == 'cite':
-                line.set_align(ALIGN['right'])
-                bbcode.append('[i]"')
-                bbcode.append('"[/i]')
-            elif soup.name == 'a':
-                pass
-                # TODO: Add link engine
-                # bbcode.append('[]')
-                # bbcode.append('[]')
-            elif soup.name == 'table':
-                pass
-                # TODO: Add table engine
-                # bbcode.append('[]')
-                # bbcode.append('[]')
-            elif soup.name == 'tr':
-                pass
-                # TODO: Add table engine
-                # bbcode.append('[]')
-                # bbcode.append('[]')
-            elif soup.name == 'th':
-                pass
-                # TODO: Add table engine
-                # bbcode.append('[]')
-                # bbcode.append('[]')
-            elif soup.name == 'td':
-                pass
-                # TODO: Add table engine
-                # bbcode.append('[]')
-                # bbcode.append('[]')
-            # elif soup.name is None:
-            #     pass
-            # elif soup.name == '':
-            #     bbcode.append('[]')
-            #     bbcode.append('[]')
-            # elif soup.name == '':
-            #     bbcode.append('[]')
-            #     bbcode.append('[]')
-            # elif soup.name == '':
-            #     bbcode.append('[]')
-            #     bbcode.append('[]')
-            # elif soup.name == '':
-            #     bbcode.append('[]')
-            #     bbcode.append('[]')
-            else:
-                pass
-
-            if type(soup) != NV:
-                old_text = soup.get_text()
-                # print(old_text)
-                text = ' '
-                for child in soup.children:
-                    new_t = self.prepare(child)
-                    # print('child %s : %s'%(child.name, new_t))
-                    if new_t is not None:
-                        removed = self.__text.pop(-1)
-                        text = old_text.replace(new_t['old'], new_t['new'])
-                text = bbcode[0] + text + bbcode[1] if bbcode != [] else text
-                line.set_tag(soup.name)
-                if line.get_text() is None:
-                    line.set_text(text)
-                self.__text.append(line)
-                return dict(old=old_text, new=text)
-            else:
-                line.set_tag('text')
-                line.set_text(soup)
-                self.__text.append(line)
-                return dict(old=soup.strip('\n'), new=soup.strip('\n'))
-        return None
+    def finish(self, soup, line, bbcode):
+        if type(soup) != NV:
+            old_text = soup.get_text()
+            text = ' '
+            for child in soup.children:
+                new_t = self.prepare(child)
+                if new_t is not None:
+                    remove = self.__text.pop(-1)
+                    text = old_text.replace(new_t['old'], new_t['new'])
+            text = bbcode[0] + text + bbcode[1] if bbcode != [] else text
+            line.set_tag(soup.name)
+            if line.get_text() is None:
+                line.set_text(text)
+            self.__text.append(line)
+            return dict(old=old_text, new=text)
+        else:
+            line.set_tag('text')
+            line.set_text(soup)
+            self.__text.append(line)
+            return dict(old=soup.strip('\n'), new=soup.strip('\n'))
 
     def get_coverpage(self):
-        response = {'title': 'Unknown', 'author': 'Unknown', 'coverimage': None}
-        title_info = self.__soup.find('title-info')
-        if title_info is not None:
-            for item in title_info.children:
-                if item.name == 'author':
-                    if item.nickname is not None:
-                        response['author'] += '(' + item.get_text() + ')'
-                    else:
-                        response['author'] = ''
-                        for child in item.children:
-                            response['author'] += child.get_text() + ' '
-                elif item.name == 'book-title':
-                    response['title'] = item.get_text()
-                elif item.name == 'coverpage':
-                    href = item.image.get('l:href')
-                    #print('href is %s' % href)
-                    if href is None:
-                        href = item.image.get('xlink:href')
-                        #print('href is %s' % href)
-                    if href[0] == "#":
-                        #href = item.image['l:href']
-                        href = href.strip('#')
-                        #print(href)
-                        cover_image = self.__soup.find('binary', attrs={'id': href})
-                        if cover_image is not None:
-                            cover_image = cover_image.get_text().strip('\n')
-                            #response['coverimage'] = cover_image
-                            response['coverimage'] = BytesIO(base64.standard_b64decode(cover_image))
-                        else:
-                            response['coverimage'] = 'default'
-        return response
+        coverpage = GetCoverImage(self.__soup)
+        return coverpage.get_response()
 
     def clear_text(self, counter):
         for item in self.__text:
-            if item.get_tag() == 'text' or item.get_text() is None:
+            if item.get_tag() == 'text' or item.get_text() is None or item.get_tag() is None:
                 self.__text.remove(item)
         if counter > 0:
             counter -= 1
             self.clear_text(counter)
+
+    def body(self, soup):
+        for child in soup.children:
+            self.prepare(child)
+        return None
+
+    def section(self, soup):
+        # TODO add sections
+        # if soup.get('id') is not None:
+        #     line.set_text(soup['id'])
+        # self.__text.append(line)
+        #return self.body(soup)
+        self.body(soup)
+        return None
+
+    def description(self, soup):
+        return None
+
+    def binary(self, soup):
+        return None
+
+    def fiction_book(self, soup):
+        self.body(soup)
+        return None
+
+    def document(self, soup):
+        self.body(soup)
+        return None
+
+    @recursive
+    def title(self, soup, line, bbcode):
+        bbcode.append('[size=%s]' % self.__title_font_size)
+        bbcode.append('[/size]')
+
+    @recursive
+    def epigraph(self, soup, line, bbcode):
+        line.set_align(ALIGN['right'])
+        bbcode.append('[i]')
+        bbcode.append('[/i]')
+
+    @recursive
+    def subtitle(self, soup, line, bbcode):
+        bbcode.append('[size=%s]' % self.__subtitle_font_size)
+        bbcode.append('[/size]')
+
+    @recursive
+    def image(self, soup, line, bbcode):
+        href = soup.get('l:href')
+        if href is None:
+            href = soup.get('xlink:href')
+        if href[0] == "#":
+            href = href.strip('#')
+            cover_image = self.__soup.find('binary', attrs={'id': href})
+            if cover_image is not None:
+                cover_image = cover_image.get_text().strip('\n')
+            else:
+                cover_image = 'default'
+            line.set_text(cover_image)
+
+    @recursive
+    def annotation(self, soup, line, bbcode):
+        bbcode.append('[i]')
+        bbcode.append('[/i]')
+
+    @recursive
+    def p(self, soup, line, bbcode):
+        bbcode.append('\t')
+        # To get not None
+        bbcode.append(' ')
+
+    @recursive
+    def empty_line(self, soup, line, bbcode):
+        bbcode.append('\n')
+        # To get not None
+        bbcode.append(' ')
+
+    @recursive
+    def strong(self, soup, line, bbcode):
+        bbcode.append('[b]')
+        bbcode.append('[/b]')
+
+    @recursive
+    def emphasis(self, soup, line, bbcode):
+        bbcode.append('[i]')
+        bbcode.append('[/i]')
+
+    @recursive
+    def sub(self, soup, line, bbcode):
+        bbcode.append('[sub]')
+        bbcode.append('[/sub]')
+
+    @recursive
+    def sup(self, soup, line, bbcode):
+        bbcode.append('[sup]')
+        bbcode.append('[/sup]')
+
+    @recursive
+    def strikethrough(self, soup, line, bbcode):
+        bbcode.append('[strikethrough]')
+        bbcode.append('[/strikethrough]')
+
+    @recursive
+    def code(self, soup, line, bbcode):
+        bbcode.append('[code]')
+        bbcode.append('[/code]')
+
+    @recursive
+    def poem(self, soup, line, bbcode):
+        line.set_align(ALIGN['center'])
+
+    @recursive
+    def stanza(self, soup, line, bbcode):
+        bbcode.append('\n')
+        bbcode.append(' ')
+
+    @recursive
+    def v(self, soup, line, bbcode):
+        line.set_align(ALIGN['center'])
+        bbcode.append('\n')
+        bbcode.append(' ')
+
+    @recursive
+    def text_author(self, soup, line, bbcode):
+        line.set_align(ALIGN['right'])
+        bbcode.append('\n')
+        bbcode.append(' ')
+
+    @recursive
+    def date(self, soup, line, bbcode):
+        line.set_align(ALIGN['right'])
+        bbcode.append('\n')
+        bbcode.append(' ')
+
+    @recursive
+    def cite(self, soup, line, bbcode):
+        line.set_align(ALIGN['right'])
+        bbcode.append('[i]"')
+        bbcode.append('"[/i]')
+
+
+class GetCoverImage:
+    def __init__(self, soup):
+        self.response = {'title': 'Unknown', 'author': 'Unknown', 'coverimage': None}
+        self.__soup = soup
+        self.title_info = self.__soup.find('title-info')
+        if self.title_info is not None:
+            self.start()
+
+    def get_response(self):
+        return self.response
+
+    def start(self):
+        options = {
+            'author': self.author,
+            'book-title': self.book_title,
+            'coverpage': self.coverpage,
+        }
+        for item in self.title_info.children:
+            options[item.name](item)
+
+    def author(self, item):
+        if item.nickname is not None:
+            self.response['author'] += '(' + item.get_text() + ')'
+        else:
+            self.response['author'] = ''
+            for child in item.children:
+                self.response['author'] += child.get_text() + ' '
+
+    def book_title(self, item):
+        self.response['title'] = item.get_text()
+
+    def coverpage(self, item):
+        href = item.image.get('l:href')
+        if href is None:
+            href = item.image.get('xlink:href')
+        if href[0] == "#":
+            href = href.strip('#')
+            cover_image = self.__soup.find('binary', attrs={'id': href})
+            if cover_image is not None:
+                cover_image = cover_image.get_text().strip('\n')
+                self.response['coverimage'] = BytesIO(base64.standard_b64decode(cover_image))
+            else:
+                self.response['coverimage'] = 'default'
 
 
 #####################################
@@ -284,9 +345,10 @@ if __name__ == '__main__':
         print('LEN IS %s' % len(parser.get_text()))
         for child in parser.get_text():
             if child.get_tag() == 'image':
-                print(child.get_text()[0])
+                logging.info(child.get_text()[0])
             else:
-                print('%s' % child.get_text())
+                logging.info('%s' % child.get_text())
+            #logging.info(child.get_tag())
         # coverpage = parser.get_coverpage()['coverimage']
 
     #from kivy.core.image import Image as CoreImage
